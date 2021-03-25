881c03d673903e3ba99718339a2d09b56dad91fe
Batuhan Hasan Akin <batuhanhasanakin@gmail.com>
2021-03-25 22:27:05 +0300

    change db model and validation

--- models/user.js
+++ models/user.js
@@ -1,15 +1,18 @@
 const userSchema = {
   $jsonSchema: {
     bsonType: "object",
-    required: ["username", "hash"],
+    required: ["email", "hash"],
     properties: {
-      username: {
+      email: {
         bsonType: "string",
         description: "required",
       },
       hash: {
         bsonType: "string",
         description: "required",
+      },
+      fullName: {
+        bsonType: "string",
       },
       isDeleted: {
         bsonType: "bool",
--- validations/userValidations.js
+++ validations/userValidations.js
@@ -1,12 +1,12 @@
 const { Joi } = require("celebrate");
 
 const JString = Joi.string();
-const JStringRequired = JString.required();
 const MongoIdSchema = JString.regex(/^[0-9a-fA-F]{24}$/);
 
 const NEW_VALIDATION = Joi.object().keys({
-  fullName: JStringRequired.min(3),
-  password: JStringRequired,
+  email: Joi.string().email(),
+  fullName: Joi.string(),
+  password: Joi.string(),
 });
 
 const GET_VALIDATION = Joi.object().keys({
